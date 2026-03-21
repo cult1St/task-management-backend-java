b211841fba368deacefa63023400744dd720f494
wea1t <cultistsffmax@gmail.com>
2026-03-21 13:30:26 +0100

    Added and completed websocket

--- src/main/java/com/task_management/first_backend/application/config/SecurityConfig.java
+++ src/main/java/com/task_management/first_backend/application/config/SecurityConfig.java
@@ -85,7 +85,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
